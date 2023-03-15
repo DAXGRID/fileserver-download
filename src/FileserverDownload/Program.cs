2814946467282d204baa62d5ad4bfab72a390acd
Rune Nielsen <runenielsen@runbox.com>
2023-03-15 16:05:07 +0100

    can now handle command line arguments

--- src/FileserverDownload/Program.cs
+++ src/FileserverDownload/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace FileServerDownload;
+﻿using CommandLine;
+
+namespace FileServerDownload;
+
+public record CommandLineOptions
+{
+    [Option('u', "username", Required = true, HelpText = "Fileserver username.")]
+    public string Username { get; init; } = "";
+
+    [Option('p', "password", Required = true, HelpText = "Fileserver password.")]
+    public string Password { get; init; } = "";
+
+    [Option('f', "fileNamePrefix", Required = true, HelpText = "Fileserver filename prefix.")]
+    public string FileNamePrefix { get; init; } = "";
+}
 
 internal static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Parser.Default
+            .ParseArguments<CommandLineOptions>(args)
+            .WithParsed<CommandLineOptions>(o =>
+            {
+                Console.WriteLine($"Username is: '{o.Username}'");
+                Console.WriteLine($"Password is: '{o.Password}'");
+                Console.WriteLine($"File name is: '{o.FileNamePrefix}'");
+            });
     }
 }
