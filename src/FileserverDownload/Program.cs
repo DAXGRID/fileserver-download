42fd5221a7cb382e2c54569b112da97f4474d94a
Rune Nielsen <runenielsen@runbox.com>
2023-03-15 16:29:10 +0100

    add resource path to command line arguments

--- src/FileserverDownload/Program.cs
+++ src/FileserverDownload/Program.cs
@@ -4,13 +4,16 @@ namespace FileServerDownload;
 
 public record CommandLineOptions
 {
+    [Option('r', "resource", Required = true, HelpText = "Fileserver resource path.")]
+    public string ResourcePath { get; init; } = "";
+
     [Option('u', "username", Required = true, HelpText = "Fileserver username.")]
     public string Username { get; init; } = "";
 
     [Option('p', "password", Required = true, HelpText = "Fileserver password.")]
     public string Password { get; init; } = "";
 
-    [Option('f', "fileNamePrefix", Required = true, HelpText = "Fileserver filename prefix.")]
+    [Option('f', "filename", Required = true, HelpText = "Fileserver filename prefix.")]
     public string FileNamePrefix { get; init; } = "";
 }
 
@@ -22,6 +25,7 @@ internal static class Program
             .ParseArguments<CommandLineOptions>(args)
             .WithParsed<CommandLineOptions>(o =>
             {
+                Console.WriteLine($"Url is: '{o.ResourcePath}'");
                 Console.WriteLine($"Username is: '{o.Username}'");
                 Console.WriteLine($"Password is: '{o.Password}'");
                 Console.WriteLine($"File name is: '{o.FileNamePrefix}'");
